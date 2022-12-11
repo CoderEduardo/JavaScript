7fbb5b6b2d299ee4098ae96bdd21708d9363a55d
CoderEduardo <luiseduardo250305@gmail.com>
2022-12-11 13:54:52 -0300

    new file:   "Concatena\303\247\303\243o/index.html" 	new file:   "Concatena\303\247\303\243o/script.js" 	new file:   "Concatena\303\247\303\243o/style.css" 	modified:   "Vari\303\241veis/index.html"

---
+++ Concatenação/script.js
@@ -0,0 +1,4 @@
+var nome = prompt('Digite o seu nome:')
+var idade = prompt('Digite a sua idade')
+
+document.write('<h1>Olá '+ nome + ', como você está? Estou vendo aqui que você tem '+idade+' anos</h1>')
