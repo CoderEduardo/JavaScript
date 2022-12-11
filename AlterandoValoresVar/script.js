3c81a132a68e979613a1a03381c90c57f22218f1
CoderEduardo <luiseduardo250305@gmail.com>
2022-12-11 14:24:20 -0300

    new file:   AlterandoValoresVar/index.html 	new file:   AlterandoValoresVar/script.js 	new file:   AlterandoValoresVar/style.css

---
+++ AlterandoValoresVar/script.js
@@ -0,0 +1,10 @@
+//Variáveis com o valor null existem, porém estão intencionalmente vazias
+var teste = null
+
+//Variáveis undefined tem o conteúdo inexistente 
+var teste2 = undefined
+var teste3 //dessas duas formas podemos definir variáveis undefined
+
+teste = 'Valor modificado com sucesso'
+
+console.log(teste)
