c862f429968cc7fa118124c8f2e16e33dc654875
CoderEduardo <luiseduardo250305@gmail.com>
2022-12-11 10:56:03 -0300

    modified:   "Vari\303\241veis/index.html" 	modified:   "Vari\303\241veis/script.js"

---
+++ Variáveis/script.js
@@ -0,0 +1,15 @@
+//Variável tipo String
+var texto = "Curso de JavaScript"
+
+//Variável tipo Number
+var numeroInteiro = 10
+var numeroFracionado = 25.61
+
+//Variável tipo boolean
+var teste = true   //true representa um valor verdadeiro e false um valor falso
+
+//alert(numeroFracionado)  //alert é uma caixa de dialogo que pode ser usado para debugar códigos
+
+//a função "document" é usada para manipular tudo que se tem em uma página html
+document.write(texto)
+
