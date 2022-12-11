fbc46d5bc274070e7a182ad0d8cb7253982c6bef
CoderEduardo <luiseduardo250305@gmail.com>
2022-12-11 13:55:51 -0300

    modified:   "Concatena\303\247\303\243o/script.js"

--- Concatenação/script.js
+++ Concatenação/script.js
@@ -1,4 +1,4 @@
 var nome = prompt('Digite o seu nome:')
-var idade = prompt('Digite a sua idade')
+var idade = prompt('Digite a sua idade:')
 
 document.write('<h1>Olá '+ nome + ', como você está? Estou vendo aqui que você tem '+idade+' anos</h1>')
