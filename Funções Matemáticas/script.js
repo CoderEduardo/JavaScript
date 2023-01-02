5cc80c55d0b073f2dd927b20baa6ddfd7b80a0c6
CoderEduardo <luiseduardo250305@gmail.com>
2023-01-02 16:23:50 -0300

    Funções matemáticas usadas para arredondar números

---
+++ Funções Matemáticas/script.js
@@ -0,0 +1,18 @@
+//O método Math.ceil() arrendonda o valor do número sempre para cima
+var x = Math.ceil(10.460)
+document.write(x + '<br>')
+
+//O método Math.floor() arredonda o valor do número sempre para baixo
+var y = Math.floor(10.460)
+document.write(y + '<br>')
+
+/*O método Math.round() arredonda de acordo com a necessidade da médida do número, se for abaixo de 5 arrendonda para baixo, 
+se for para cima de 5 arrendonda para cima.*/
+var a = Math.round(10.580)
+document.write(a + '<br>')
+var a = Math.round(10.450)
+document.write(a)
+
+//O método Math.random() gera número aleatórios
+var b = Math.random()
+document.write(b)
