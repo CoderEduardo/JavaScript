21b674801f43c7218c69ab1a7e5324ab370d6fed
CoderEduardo <luiseduardo250305@gmail.com>
2023-01-02 14:11:21 -0300

    Escopo de Variáveis

---
+++ EscopoDeVariáveis/script.js
@@ -0,0 +1,8 @@
+/*Existem 3 tipos de escopos de variáveis, o escopo global, função e bloco
+O global se refere a todo o arquivo Js
+A função se refere a todas variáveis dentro de uma função
+E o bloco se refere a todos as variáveis contidas dentro de comandos, como if e else, switch*/
+
+/*Variáveis declaradas dentro do escopo global podem ser utilizadas dentro de funções e blocos.
+Variáveis declaradas dentro de blocos sofrem elevações e podem ser utilizadas em funções e no escopo global
+Variáveis declaradas dentro de funções não podem ser utilizadas no escopo de bloco e no global*/
