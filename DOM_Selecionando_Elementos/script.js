a24d7609568d50c41ed354321aef43e3d8713945
CoderEduardo <luiseduardo250305@gmail.com>
2023-01-03 11:20:11 -0300

    Selecionando os elementos no DOM

---
+++ DOM_Selecionando_Elementos/script.js
@@ -0,0 +1,7 @@
+//document.getElementById('nome_usuario') Seleciona o elemento no DOM através do id
+
+//document.getElementsByTagName('input') Seleciona o elemento no DOM através da tag
+
+//document.getElementsByClassName('campo_texto') Seleciona o elemento no DOM através da classe
+
+//document.getElementsByName('nome') Seleciona o elemento no DOM através do name
