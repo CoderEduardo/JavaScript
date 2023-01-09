8fab966a89d3198f28fa761439ef60da94755b90
CoderEduardo <luiseduardo250305@gmail.com>
2023-01-09 18:14:07 -0300

    Arrays Básicos

---
+++ Arrays/Array Básico/script.js
@@ -0,0 +1,12 @@
+//var lista_frutas = Array()
+var lista_frutas = []
+
+//var lista_frutas = Array('Banana', 'Maça','Morango','Uva') Outra forma de declarar Arrays, o Js entende a declaração partindo do índice 0 
+//var lista_frutas = ['Banana', 'Maça', 'Morango', 'Uva'] 
+
+lista_frutas[1] = 'Banana'
+lista_frutas[2] = 'Maça'
+lista_frutas[3] = 'Morango'
+lista_frutas[4] = 'Uva'
+
+console.log(lista_frutas[1])
