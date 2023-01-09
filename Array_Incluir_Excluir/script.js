fc4a5011cc5ce4a14a504a4d9be30d5208f2b4ed
CoderEduardo <luiseduardo250305@gmail.com>
2023-01-09 20:10:44 -0300

    Incluindo e Deletando Arrays

---
+++ Array_Incluir_Excluir/script.js
@@ -0,0 +1,20 @@
+var lista_coisas = Array()
+
+lista_coisas['frutas'] = Array()
+lista_coisas['pessoas'] =[]
+
+lista_coisas['frutas'].push('Banana')
+
+/*Incluir um Elemento no final do Array
+lista_frutas.push("Uva")*/
+
+/*Incluir um elemento no começo do Array*/
+//lista_frutas.unshift("Uva")
+
+/*Excluir elementos no final de um Array*/
+//lista_frutas.pop()
+
+/*Excluir elementos no começo de um Array*/
+//lista_frutas.shift()
+
+console.log(lista_frutas)
