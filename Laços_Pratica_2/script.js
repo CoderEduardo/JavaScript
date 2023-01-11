c18935ef6921d4609926b7622d4695eb79c7d8f7
CoderEduardo <luiseduardo250305@gmail.com>
2023-01-11 18:07:47 -0300

    Criando uma tabuada de 1 a 10 com for

---
+++ Laços_Pratica_2/script.js
@@ -0,0 +1,11 @@
+for (var y = 1; y <= 10; y++) {
+
+    for (var x = 1; x <= 10; x++) {
+        document.write(y + " X " + x + " = " + (x * y) + "<br/>")
+    }
+
+    document.write("<hr/>")
+    
+}
+
+
