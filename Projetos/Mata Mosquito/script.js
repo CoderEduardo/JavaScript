811840d60f36e486aeab251ce4dcc6cf4a21af76
CoderEduardo <luiseduardo250305@gmail.com>
2023-01-12 11:12:01 -0300

    renamed:    Projetos/Mata Mosquito/imagens/mosca.png -> Projetos/Mata Mosquito/imagens/mosquito.png 	modified:   Projetos/Mata Mosquito/index.html 	modified:   Projetos/Mata Mosquito/script.js 	modified:   Projetos/Mata Mosquito/style.css

--- Projetos/Mata Mosquito/script.js
+++ Projetos/Mata Mosquito/script.js
@@ -12,3 +12,25 @@ ajustarTamanho()
 
 /** ************************************************************************************************************************************************/
 
+/**Função Criada para movitar o mosquito de forma aleatória na página*/
+
+function posicaoRandomica() {
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    /*como é subtraido os 90 quando o valor 
+    for  ficaria -90, por isso esse esquema
+    para ele recever o seu proprio valor*/
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    var mosquito = document.createElement('img')
+    mosquito.src = "imagens/mosquito.png"
+    mosquito.className = 'mosquito1'
+    //implementado para a aleatóriedade funcionar
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
