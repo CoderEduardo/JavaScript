aa8a6709d7d1a7cdbcdd65d185c47c6b2c06a0b5
CoderEduardo <luiseduardo250305@gmail.com>
2023-01-12 12:40:48 -0300

    Aplicando um tamanho aleatório para o mosquito

--- Projetos/Mata Mosquito/script.js
+++ Projetos/Mata Mosquito/script.js
@@ -26,11 +26,30 @@ function posicaoRandomica() {
 
     var mosquito = document.createElement('img')
     mosquito.src = "imagens/mosquito.png"
-    mosquito.className = 'mosquito1'
+    mosquito.className = tamanhoAleatorio()
     //implementado para a aleatóriedade funcionar
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
 
     document.body.appendChild(mosquito)
+
+    tamanhoAleatorio()
+}
+
+/******************************************************************************************************************* */
+
+//Aplica um tamanho aleatório para o mosquito   
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch (classe) {
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
 }
