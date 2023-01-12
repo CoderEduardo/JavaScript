6c1de005e350efb3551eb8db2ace8903ac74489e
CoderEduardo <luiseduardo250305@gmail.com>
2023-01-12 10:42:53 -0300

    Definindo a dimensão atual do palco do jogo

---
+++ Projetos/Mata Mosquito/script.js
@@ -0,0 +1,16 @@
+/* Esquema feito para sempre capturar o tamanho atual da janela da página, com a implementação do onresize no body chamando a função ajustarTamanho()*/
+
+var altura = 0
+var largura = 0
+
+function ajustarTamanho() {
+    altura = innerHeight
+    largura = innerWidth
+    
+    console.log(altura, largura)
+}
+
+ajustarTamanho()
+
+/** ************************************************************************************************************************************************/
+
