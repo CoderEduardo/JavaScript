2c0080dc6e39d426b28345d660147d7b5eb07005
CoderEduardo <luiseduardo250305@gmail.com>
2023-01-31 16:38:00 -0300

    Finalizando o Game Mata Mosquito

--- Projetos/Mata Mosquito/script.js
+++ Projetos/Mata Mosquito/script.js
@@ -5,6 +5,23 @@ var largura = 0
 var vidas = 1
 var tempo = 15
 
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', "")
+
+if (nivel === 'normal') {
+    criaMosquitoTempo = 1500
+} 
+else if (nivel === 'dificil') 
+{
+    criaMosquitoTempo = 1000
+} 
+else if (nivel === 'chucknorris') 
+{
+    criaMosquitoTempo = 750
+}
+
 function ajustarTamanho() {
     altura = innerHeight
     largura = innerWidth
@@ -12,18 +29,18 @@ function ajustarTamanho() {
 
 ajustarTamanho()
 
-var cronometro = setInterval(function(){
+var cronometro = setInterval(function () {
 
     tempo -= 1
 
-    if(tempo<0){
+    if (tempo < 0) {
         clearInterval(cronometro)
         clearInterval(criaMosquito)
-        window.location.href="vitoria.html"
-    } else{
+        window.location.href = "vitoria.html"
+    } else {
         document.getElementById('cronometro').innerHTML = tempo
     }
-    
+
 }, 1000)
 
 /** ************************************************************************************************************************************************/
@@ -37,7 +54,7 @@ function posicaoRandomica() {
         document.getElementById("mosquito").remove()
 
         if (vidas > 3) {
-            window.location.href ='gameOver.html'
+            window.location.href = 'gameOver.html'
         }
         else {
             document.getElementById('v' + vidas).src = 'imagens/coracao_vazio.png'
